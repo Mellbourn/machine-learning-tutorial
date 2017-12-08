da7a24a95a1e361249fabfd5fbff4a528c980e74
Klas Mellbourn <klas@mellbourn.net>
2017-12-08 16:28:15 +0100

    ignore uninteresting warnings

--- test_tensorflow.py
+++ test_tensorflow.py
@@ -23,7 +23,7 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)  # reset values to wrong
 for i in range(1000):
-  sess.run(train, {x: x_train, y: y_train})
+    sess.run(train, {x: x_train, y: y_train})
 
 # evaluate training accuracy
 curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
